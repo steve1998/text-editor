a16d93dbe4e580a0fdcea57b2c67a9aab764174a
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-08-31 16:05:39 -0700

    Implement Fetch Files Frontend

--- src/client/services/fetchAPI.js
+++ src/client/services/fetchAPI.js
@@ -1,3 +1,15 @@
+import { FILES_URI } from  '../constants/URI'
+
+const axios = require('axios')
+
+// GET files from MongoDB
 export const fetchFiles = () => {
-    return 0
+    return axios.get(FILES_URI)
+    .then(res => {
+        let result = res.data
+        return result
+    })
+    .catch(error => {
+        console.log('GET error: ', error)
+    })
 }
