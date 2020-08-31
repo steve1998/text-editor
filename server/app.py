7b13da9295e7dedba17c042ac8997bbc80806bde
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-08-31 15:33:45 -0700

    Implement GET for Files

--- server/app.py
+++ server/app.py
@@ -1,23 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from os import environ
 import pymongo
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 # Intialize DB
-test = environ.get('TEST_VAR_1')
+db_username = environ.get('NO_SQL_USER')
+db_password = environ.get('NO_SQL_PASS')
+db_cluster_url = environ.get('NO_SQL_CLUSTER_URL')
 
-username = environ.get('NO_SQL_USER')
-password = environ.get('NO_SQL_PASSWORD')
-cluster_url = environ.get('CLUSTER_URL')
+if not all([db_username, db_password, db_cluster_url]):
+    print(db_cluster_url)
+    print(db_password)
+    print(db_username)
+    raise Exception('Missing Database Environment Variables')
+    
+client_string = 'mongodb+srv://' + db_username + ':' + db_password + '@' + db_cluster_url + '/test?retryWrites=true&w=majority'
+client = pymongo.MongoClient(client_string)
 
-# client = pymongo.MongoClient("mongodb+srv://<username>:<password>@<cluster-url>/test?retryWrites=true&w=majority")
+db = client['text-editor']
 
 @app.route("/")
 def home():
     return app.send_static_file('index.html')
 
 # Routes
+@app.route("/files", methods=['GET'])
+def get_files():
+    files_collection = db['files']
+    output = []
+
+    for document in files_collection.find():
+        output.append({'id': document['id'], 'fileName': document['fileName'], 'text': document['text']})
+
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run()
